93118757518c5a89fa3b3529355f8d647904181d
jocelynrem <jocelynrem@gmail.com>
2021-06-06 23:50:36 -0400

    open github in new tab

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
-const { title } = require("process");
-
 
 const questions = [
     {
