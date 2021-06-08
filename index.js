959c3cc259332fe590f2ae5685d214a471d2f807
jocelynrem <jocelynrem@gmail.com>
2021-06-07 20:20:20 -0400

    separated manager questions

--- index.js
+++ index.js
@@ -2,18 +2,48 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const validateEmail = require("js-email-validator");
 
-
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 
-const questions = [
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Manager name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Manager ID:',
+    },
+
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: "Manager Office number:",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Manager email:',
+        validate: (value) => {
+            if (!validateEmail(value)) {
+                console.log(`\n Please enter a valid response.`)
+            } else {
+                return true;
+            }
+        }
+    }
+
+]
+const teamQuestions = [
     {
         type: 'list',
         name: 'title',
         message: "Employee Title:",
-        choices: ['Manager', 'Engineer', 'Intern'],
+        choices: ['Engineer', 'Intern'],   
     },
     {
         type: 'input',
@@ -24,13 +54,6 @@ const questions = [
         type: 'input',
         name: 'id',
         message: 'Employee ID:',
-    },
-
-    {
-        type: 'input',
-        name: 'officeNum',
-        message: "Office number:",
-        when: (answers) => answers.title === 'Manager'
     },
     {
         type: 'input',
@@ -48,11 +71,40 @@ const questions = [
         type: 'input',
         name: 'email',
         message: 'Employee email:',
-        validate: validateEmail
-    },
+        validate: (value) => {
+            if (!validateEmail(value)) {
+                console.log(`\n Please enter a valid response.`)
+            } else {
+                return true;
+            }
+        }
+    }
 ]
-const openHTML = () => {
-    const oHTML = `<!DOCTYPE html>
+
+const startGen = () => {
+    inquirer.prompt(managerQuestions).then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.officeNum, answers.email)
+        managerHTML(manager);
+        addOrDone();
+    })
+}
+
+const newTeamMember = () => {
+    inquirer.prompt(teamQuestions).then((answers) => {
+        if (answers.title === 'Engineer') {
+            const engineer = new Engineer(answers.name, answers.id, answers.github, answers.email)
+            engineerHTML(engineer);
+        } else {
+            const intern = new Intern(answers.name, answers.id, answers.school, answers.email)
+            internHTML(intern)
+        }
+        addOrDone();
+    })
+}
+
+const managerHTML = (manager) => {
+    const mHTML = `
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -71,61 +123,38 @@ const openHTML = () => {
         </div>
     </div>
     <div class=container>
-        <div class="row">`
-    fs.writeFile('./dist/team.html', oHTML, (err) => err ? console.log(err) : '')
-
-}
-const newTeamMember = () => {
-    inquirer.prompt(questions).then((answers) => {
-        if (answers.title === 'Manager') {
-            const manager = new Manager(answers.name, answers.id, answers.officeNum, answers.email)
-            managerHTML(manager);
-        } else if (answers.title === 'Engineer') {
-            const engineer = new Engineer(answers.name, answers.id, answers.github, answers.email)
-            engineerHTML(engineer);
-        } else {
-            const intern = new Intern(answers.name, answers.id, answers.school, answers.email)
-            internHTML(intern)
-        }
-        addOrDone();
-    })
-}
-
-const managerHTML = (manager) => {
-    const { name, id, officeNum, email } = manager
-    const mHTML = `
+        <div class="row">
             <div class =col>
-                <div class="card" style="width: 13rem;">
+                <div class="card" style="width: 15rem;">
                     <div class="card-header bg-dark text-white">
                         <h4>Manager</h4>
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">${name}</h5>
+                        <h5 class="card-title">${manager.getName()}</h5>
                         <ul>
-                            <li>ID: ${id}</li>
-                            <li>Office: ${officeNum}</li>
-                            <li><a href="mailto:${email}" class="card-link">${email}</a></li>
+                            <li>ID: ${manager.getId()}</li>
+                            <li>${manager.getOfficeNum()}</li>
+                            <li><a href="mailto:${manager.getEmail()}" class="card-link">${manager.getEmail()}</a></li>
                         </ul>
                     </div>
                 </div>
             </div>`
-    fs.appendFile('./dist/team.html', mHTML, (err) => err ? console.log(err) : '')
+    fs.writeFile('./dist/team.html', mHTML, (err) => err ? console.log(err) : '')
 };
 
 const engineerHTML = (engineer) => {
-    const { name, id, github, email } = engineer
     const eHTML = `
     <div class="col">
-        <div class="card" style="width: 13rem;">
+        <div class="card" style="width: 15rem;">
             <div class="card-header bg-primary text-white">
                 <h4>Engineer</h4>
             </div>
             <div class="card-body">
-                <h5 class="card-title">${name}</h5>
+                <h5 class="card-title">${engineer.getName()}</h5>
                 <ul>
-                    <li>ID: ${id}</li>
-                    <li><a href="https://github.com/${github}" target="_blank" class="card-link">GitHub: ${github}</a></li>
-                    <li><a href="mailto:${email}" class="card-link">${email}</a></li>
+                    <li>ID: ${engineer.getId()}</li>
+                    <li><a href="https://github.com/${engineer.getGithub()}" target="_blank" class="card-link">GitHub: ${engineer.getGithub()}</a></li>
+                    <li><a href="mailto:${engineer.getEmail()}" class="card-link">${engineer.getEmail()}</a></li>
                 </ul>
             </div>
         </div>
@@ -134,19 +163,18 @@ const engineerHTML = (engineer) => {
 };
 
 const internHTML = (intern) => {
-    const { name, id, school, email } = intern
     const iHTML = `
     <div class="col">
-        <div class="card" style="width: 13rem;">
+        <div class="card" style="width: 15rem;">
             <div class="card-header bg-info text-white">
                 <h4>Intern</h4>
             </div>
             <div class="card-body">
-                <h5 class="card-title">${name}</h5>
+                <h5 class="card-title">${intern.getName()}</h5>
                 <ul>
-                    <li>ID: ${id}</li>
-                    <li>${school}</li>
-                    <li><a href="mailto:${email}" class="card-link">${email}</a></li>
+                    <li>ID: ${intern.getId()}</li>
+                    <li>${intern.getSchool()}</li>
+                    <li><a href="mailto:${intern.getEmail()}" class="card-link">${intern.getEmail()}</a></li>
                 </ul>
             </div>
         </div>
@@ -178,5 +206,4 @@ const addOrDone = () => {
     });
 };
 
-openHTML();
-newTeamMember();
+startGen();
